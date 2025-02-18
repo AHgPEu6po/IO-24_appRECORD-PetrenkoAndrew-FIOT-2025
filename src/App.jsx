571d79a4062c7540310801b533423b33b6828cd2
AHgPEu6po <andrewpetrenko2005@gmail.com>
2025-02-18 19:02:29 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navigation from "./components/Navigation.jsx";
+import Laboratorna1 from "./pages/Laboratorna_1.jsx";
+import Laboratorna2 from "./pages/Laboratorna_2.jsx";
+import Laboratorna3 from "./pages/Laboratorna_3.jsx";
+import Laboratorna4 from "./pages/Laboratorna_4.jsx";
+import Laboratorna5 from "./pages/Laboratorna_5.jsx";
+import Laboratorna6 from "./pages/Laboratorna_6.jsx";
+import Laboratorna7 from "./pages/Laboratorna_7.jsx";
+import Laboratorna8 from "./pages/Laboratorna_8.jsx";
+import Laboratorna9 from "./pages/Laboratorna_9.jsx";
+import MainText from "./components/MainText.jsx";
+
+
+const MyComponent = () => {
+    return (
+        <Router>
+            <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+                <MainText/>
+                <Navigation/>
+                <Routes>
+                    <Route path="/IO-24_appRECORD-PetrenkoAndrew-FIOT-2025/l1" element={<Laboratorna1/>} />
+                    <Route path="/IO-24_appRECORD-PetrenkoAndrew-FIOT-2025/l2" element={<Laboratorna2/>} />
+                    <Route path="/IO-24_appRECORD-PetrenkoAndrew-FIOT-2025/l3" element={<Laboratorna3/>} />
+                    <Route path="/IO-24_appRECORD-PetrenkoAndrew-FIOT-2025/l4" element={<Laboratorna4/>} />
+                    <Route path="/IO-24_appRECORD-PetrenkoAndrew-FIOT-2025/l5" element={<Laboratorna5/>} />
+                    <Route path="/IO-24_appRECORD-PetrenkoAndrew-FIOT-2025/l6" element={<Laboratorna6/>} />
+                    <Route path="/IO-24_appRECORD-PetrenkoAndrew-FIOT-2025/l7" element={<Laboratorna7/>} />
+                    <Route path="/IO-24_appRECORD-PetrenkoAndrew-FIOT-2025/l8" element={<Laboratorna8/>} />
+                    <Route path="/IO-24_appRECORD-PetrenkoAndrew-FIOT-2025/l9" element={<Laboratorna9/>} />
+                </Routes>
+            </div>
+        </Router>
+    );
+};
+
+export default MyComponent;
---
+++ src/assets/assets.js
@@ -0,0 +1,21 @@
+import menu from './Images/menu.svg'
+import dropdown from './Images/dropdown.svg'
+import me from './Images/me.png'
+
+import list_photo from './Images/list_photo.png'
+import list_code from './Images/list_code.png'
+import image_photo from './Images/image_photo.png'
+import image_code from './Images/image_code.png'
+import table_photo from './Images/table_photo.png'
+import table_code from './Images/table_code.png'
+export const assets = {
+    dropdown,
+    menu,
+    me,
+    list_photo,
+    list_code,
+    image_photo,
+    image_code,
+    table_photo,
+    table_code
+}
