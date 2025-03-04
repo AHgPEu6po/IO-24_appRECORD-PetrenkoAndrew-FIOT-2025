d4f4f7a059652e72d32fd5109c442d9a9873da0c
AHgPEu6po <andrewpetrenko2005@gmail.com>
2025-03-04 12:47:11 +0200

    second commit

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,13 +1,29 @@
-import menu from './Images/menu.svg'
-import dropdown from './Images/dropdown.svg'
-import me from './Images/me.png'
+import menu from './images/menu.svg'
+import dropdown from './images/dropdown.svg'
+import me from './images/me.png'
 
-import list_photo from './Images/list_photo.png'
-import list_code from './Images/list_code.png'
-import image_photo from './Images/image_photo.png'
-import image_code from './Images/image_code.png'
-import table_photo from './Images/table_photo.png'
-import table_code from './Images/table_code.png'
+// лабораторна 1
+import list_photo from './images/laboratorna_1/list_photo.png'
+import list_code from './images/laboratorna_1/list_code.png'
+import image_photo from './images/laboratorna_1/image_photo.png'
+import image_code from './images/laboratorna_1/image_code.png'
+import table_photo from './images/laboratorna_1/table_photo.png'
+import table_code from './images/laboratorna_1/table_code.png'
+
+// лабораторна 2
+import tag_selector from './images/laboratorna_2/tag_selector.png'
+import tag_selector_photo from './images/laboratorna_2/tag_selector_photo.png'
+import class_selector from './images/laboratorna_2/class_selector.png'
+import class_selector_photo from './images/laboratorna_2/class_selector_photo.png'
+import font from './images/laboratorna_2/font.png'
+import table_lab2 from './images/laboratorna_2/table_lab2.png'
+import table_lab2_photo from './images/laboratorna_2/table_lab2_photo.png'
+import background from './images/laboratorna_2/background.png'
+import text_color from './images/laboratorna_2/text_color.png'
+import text_color_photo from './images/laboratorna_2/text_color_photo.png'
+import list_lab2 from './images/laboratorna_2/list_lab2.png'
+import list_lab2_photo from './images/laboratorna_2/list_lab2_photo.png'
+
 export const assets = {
     dropdown,
     menu,
@@ -17,5 +33,17 @@ export const assets = {
     image_photo,
     image_code,
     table_photo,
-    table_code
+    table_code,
+    tag_selector,
+    tag_selector_photo,
+    class_selector,
+    class_selector_photo,
+    font,
+    table_lab2,
+    table_lab2_photo,
+    background,
+    text_color,
+    text_color_photo,
+    list_lab2,
+    list_lab2_photo
 }
